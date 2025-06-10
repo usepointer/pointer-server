575c55dfd10e7524979b72ecef2985fae6358b6d
Yossi Gartner <yosigartner@gmail.com>
2025-06-10 17:22:53 +0300

    add v2 for get-insights

--- src/insights/flow/insights-flow.ts
+++ src/insights/flow/insights-flow.ts
@@ -2,17 +2,39 @@ import OpenAI from "openai";
 import { Stream } from "openai/core/streaming";
 import { ResponseStreamEvent } from "openai/resources/responses/responses";
 import handlebars from 'handlebars'
-import { defaultPrompt } from "../prompts/default.prompt";
+import { defaultPrompt, defaultPromptV2 } from "../prompts/default.prompt";
 
 export class InsightsFlow {
     constructor(private readonly openai: OpenAI) { }
     async getInsights(contents: string[], customPrompt?: string): Promise<Stream<ResponseStreamEvent>> {
-        
+
         const responseStream = await this.openai.responses.create({
             model: "gpt-4.1-mini",
             instructions: handlebars.compile(defaultPrompt)({ customPrompt }),
             input: JSON.stringify(contents),
             stream: true
+        });
+
+        return responseStream
+    }
+
+    async getInsightsV2(contents: string[], visibleView: string, customPrompt?: string): Promise<Stream<ResponseStreamEvent>> {
+
+        const responseStream = await this.openai.responses.create({
+            model: "gpt-4.1-mini",
+            instructions: handlebars.compile(defaultPromptV2)({ customPrompt }),
+            input: [{
+                role: 'user',
+                content: [{
+                    type: 'input_image',
+                    detail: 'auto',
+                    image_url: visibleView
+                }, {
+                    type: 'input_text',
+                    text: JSON.stringify(contents)
+                }],
+            }],
+            stream: true,
         });
 
         return responseStream
--- src/insights/prompts/default.prompt.ts
+++ src/insights/prompts/default.prompt.ts
@@ -12,7 +12,7 @@ export const defaultPrompt = `
         Custom prompt: {{customPrompt}}
 
         ### Input Format:
-        The input will be an image of the rendered website.
+        The input will be an array of HTML content of paragraphs and spans to analyze.
 
         ### Output Format:
         The output should be markdown format, please don't include the prefix of \`\`\`markdown\`\`\` in your response."
@@ -22,3 +22,29 @@ export const defaultPrompt = `
         - Use as many markdown features as possible to enhance readability.
         - Add icons to enhance the visual appeal of the summary.      
         `
+
+export const defaultPromptV2 = `
+        ### Default Instructions:
+        You are an expert in summarizing and extracting insights from HTML content.
+        Your task is to analyze the provided HTML paragraphs and generate a concise summary with key insights.
+        Focus on the main themes, important details, and any actionable insights that can be derived from the content.
+        Ensure that the summary is clear, concise, and captures the essence of the provided content.
+        Overall, aim to provide a short as possible summary that highlights the most important aspects of the HTML content.
+
+        ### Custom Instructions:
+        In some cases a custom-promt will be provided, if so, please use it to guide your response and use it instead of the Defulat Instructions.
+        If the custom prompt asks a specific question, ensure that your response addresses that question directly and firstly.
+        Custom prompt: {{customPrompt}}
+
+        ### Input Format:
+        - Base64 encoded image of the rendered website.
+        - An array of HTML content of paragraphs and spans to analyze.
+
+        ### Output Format:
+        The output should be markdown format, please don't include the prefix of \`\`\`markdown\`\`\` in your response."
+
+        ### Important Notes:
+        - Each response should have a title that summarizes the main theme of the content.
+        - Use as many markdown features as possible to enhance readability.
+        - Add icons to enhance the visual appeal of the summary. 
+`
